9a3b99da7ef144666bc203f58e464ca38ebc9a1e
income3000 <income3000@gmail.com>
2021-10-17 12:53:13 -0400

    css

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,23 @@ import React from 'react'
 import './skills.css'
 export default function Home() {
     return (
-        <div>
+        <div className="canvas">
              
      <h2>Skills Page</h2>
     
          
             <a href="https://www.comptia.org/certifications/a" rel="noerror"><p id="1">A+</p></a> 
-            <p id="2">HTML/CSS</p>
-            <a href="http://www.income3000.com" rel="noerror"><p id="3">JavaScript</p></a>
-            <a href="https://codeblue.netlify.app/" rel="_blank" alt="nopage"><p id="4">React</p></a>
-            <p id="5">Python</p>
-            <p id="6">Django</p>
-            <p id="7">Ruby</p>
-            <a href="https://rbcontact.herokuapp.com" rel="noreferror"><p id="8">Ruby on Rails</p></a>
-            <p id="9">Express</p>
-            <p id="10">MongoDB</p>
-            <p id="11">Network +</p>
-            <p id="12">Securities +</p>
+            <a href="http://www.income3000.com" rel="noerror"><p id="1">HTML/CSS</p></a>
+            <a href="http://www.income3000.com" rel="noerror"><p id="1">JavaScript</p></a>
+            <a href="https://codeblue.netlify.app/" rel="_blank" alt="nopage"><p id="1">React</p></a>
+            <p id="1">Python</p>
+            <p id="1">Django</p>
+            <p id="1">Ruby</p>
+            <a href="https://rbcontact.herokuapp.com" rel="noreferror"><p id="1">Ruby on Rails</p></a>
+            <p id="1">Express</p>
+            <p id="1">MongoDB</p>
+            <p id="1">Network +</p>
+            <p id="1">Securities +</p>
       
         </div>
     )
